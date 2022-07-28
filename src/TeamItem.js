6290d34cf2c9fc1bb3ae0adc97a19c7f2673ce76
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-07-28 17:03:19 -0500

    begin drilling into teamStats

--- src/TeamItem.js
+++ src/TeamItem.js
@@ -1,18 +1,27 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import axios from 'axios';
 
 function TeamItem({ team }) {
 
+    const dispatch = useDispatch();
     const [teamStats, setTeamStats] = useState([]);
 
-    
+    const setStats = () => {
+        dispatch({
+            type: 'SET_STATS',
+            payload: teamStats
+        })
+    }
+
+
     const getStats = () => {
         console.log('get stats clicked');
         axios.get(`https://statsapi.web.nhl.com/api/v1/teams/${team.id}?expand=team.stats`).then(res => {
-            setTeamStats(res.data.teams)
+            console.log(res.data.teams[0].teamStats);
 
         })
-        getStats();
+        setStats();
         
         
     }
