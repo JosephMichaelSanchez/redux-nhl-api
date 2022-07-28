6bc4bf20bc2b7de698661188fceeea92d1abf0bd
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-07-28 12:28:29 -0500

    Add TeamsList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import TeamsList from './TeamsList';
 
 function App() {
   return (
     <div className="App">
       <h3>NHL API</h3>
+      <TeamsList />
     </div>
   );
 }
---
+++ src/TeamsList.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+function TeamsList() {
+
+    return (
+        <>
+            <h3>TeamsList goes here:</h3>
+        </>
+    )
+}
+
+export default TeamsList;
